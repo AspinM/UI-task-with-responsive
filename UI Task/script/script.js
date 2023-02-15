4223f10d260f52611f1ce29d0f407e39b56a862c
Aspin M <115536818+AspinM@users.noreply.github.com>
2023-02-15 15:57:23 +0530

    Add files via upload

---
+++ UI Task/script/script.js
@@ -0,0 +1,5 @@
+const myfunction=()=>{
+    let user=document.getElementById("user").value;
+    let password=document.getElementById("password").value;
+    alert("user name="+user+"\n"+"password="+password);
+}
